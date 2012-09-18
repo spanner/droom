966171550fb5d53260da5e94af456ddb99ba0430
mike1104 <mike@spanner.org>
2012-09-18 13:02:24 +0100

    directory

--- app/controllers/droom/people_controller.rb
+++ app/controllers/droom/people_controller.rb
@@ -2,9 +2,25 @@ module Droom
   class PeopleController < Droom::EngineController
     respond_to :json, :html, :js
     before_filter :authenticate_user!  
+    before_filter :find_people  
     
     def index
-      @people = Person.all
+      respond_with @people
+    end
+    
+  protected
+    
+    def find_people
+      @people = Person.scoped({})
+
+      unless params[:q].blank?
+        @searching = true
+        @people = @people.name_matching(params[:q])
+      end
+      
+      @show = params[:show] || 10
+      @page = params[:page] || 1
+      @people.page(@page).per(@show)
     end
  
   end
