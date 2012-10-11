fbed17d2f5307ddc903a056dce549c4ab86f0fcc
William Ross <will@spanner.org>
2012-10-11 12:43:08 +0100

    more dav debugging

--- lib/droom/dav_resource.rb
+++ lib/droom/dav_resource.rb
@@ -9,28 +9,35 @@
 #
 module Droom
   class DavResource < DAV4Rack::FileResource
+    attr_accessor :person
     
     # The _DAV_ prefix is a way to evade the callback mechanism.
     # Prepare is only called by our fork of Dav4Rack, at the moment.
     # 
     def prepare
-      @person = user.person if user
-      raise Dav4Rack::Unauthorized unless @person
-      @person.gather_and_update_documents if path.blank?
+      Rails.logger.warn ">>> in prepare: user is #{user} and person is #{person}"
+      raise Dav4Rack::Unauthorized unless person
+      if path.blank?  # any request for the root resource
+        person.gather_and_update_documents
+      end
     end
 
     def root
+      Rails.logger.warn ">>> in root: user is #{user} and person is #{person}"
+      
       unless @dav_root
-        @dav_root = Rails.root + "#{Droom.dav_root}/#{@person.id}"
+        @dav_root = Rails.root + "#{Droom.dav_root}/#{person.id}"
         FileUtils.mkdir_p(@dav_root) unless File.exist?(@dav_root)
       end
-      Rails.logger.warn "dav_root: #{@dav_root}"
+      Rails.logger.warn ">>> dav_root: #{@dav_root}"
       @dav_root
     end
   
     def authenticate(email, password)
       self.user = User.find_by_email(email)
-      user.try(:valid_password?, password)
+      if user.try(:valid_password?, password)
+        self.person = user.person
+      end
     end
 
   end
