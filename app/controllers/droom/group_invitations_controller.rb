1e2ebeb2018de6433a52709c8bac0736accfd6e4
mike1104 <mike@spanner.org>
2012-10-12 11:01:36 +0100

    inviting and uninviting of people and groups working

--- app/controllers/droom/group_invitations_controller.rb
+++ app/controllers/droom/group_invitations_controller.rb
@@ -1,12 +1,14 @@
 module Droom
   class GroupInvitationsController < Droom::EngineController
     respond_to :js, :html
+    layout :no_layout_if_pjax
     
+    before_filter :get_event
     before_filter :build_group_invitation, :only => [:new, :create]
     before_filter :get_group_invitation, :only => :destroy
 
     def destroy
-      @group_invitation.destroy!
+      @group_invitation.destroy
       head :ok
     end
     
@@ -30,12 +32,15 @@ module Droom
     protected
     
     def build_group_invitation
-      @event = Droom::Event.find(params[:event_id])
       @group_invitation = @event.group_invitations.new(params[:group_invitation])
     end
 
     def get_group_invitation
-      @group_invitation = Droom::GroupInvitation.find(params[:id])
+      @group_invitation = @event.group_invitations.find(params[:id])
+    end
+    
+    def get_event
+      @event = Droom::Event.find(params[:event_id])
     end
 
   end
--- app/models/droom/invitation.rb
+++ app/models/droom/invitation.rb
@@ -6,6 +6,7 @@ module Droom
     belongs_to :event
     belongs_to :group_invitation
     belongs_to :created_by, :class_name => "User"
+    validates_uniqueness_of :person_id, :scope => [:event_id, :group_invitation_id]
 
   end
 end
