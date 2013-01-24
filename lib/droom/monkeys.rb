f23226d02f2c694fbe68a3320e774a843c051f23
William Ross <will@spanner.org>
2013-01-24 11:36:14 +0000

    default user settings

--- lib/droom/monkeys.rb
+++ lib/droom/monkeys.rb
@@ -11,3 +11,16 @@ class Array
     end
   end
 end
+
+class Hash
+  def deep_set(path, value)
+    keys = path.is_a?(Array) ? path : path.to_s.split(':')
+    key = keys.shift
+    if keys.empty?
+      self[key.to_sym] = value
+    else
+      self[key.to_sym] ||= {}
+      self[key.to_sym].deep_set(keys, value)
+    end
+  end
+end
