e180e15c123717c9fec3f4d76ffd84e2b4b5a267
William Ross <will@spanner.org>
2013-03-06 18:29:18 +0000

    group and personal invitations update page correctly

--- app/models/droom/group.rb
+++ app/models/droom/group.rb
@@ -35,10 +35,14 @@ module Droom
 
     scope :visible_to, lambda { |person|
       if person
-        select('droom_groups.*')
-          .joins('INNER JOIN droom_memberships as dm on droom_groups.id = dm.group_id')
-          .where(['dm.person_id = ?', person.id])
-          .group('droom_groups.id')
+        if person.admin?
+          scoped({})
+        else
+          select('droom_groups.*')
+            .joins('INNER JOIN droom_memberships as dm on droom_groups.id = dm.group_id')
+            .where(['dm.person_id = ?', person.id])
+            .group('droom_groups.id')
+        end
       else
         where("1=0")
       end
