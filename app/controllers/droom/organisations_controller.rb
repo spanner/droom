66b0dba644fcfe43487247307ec2a3fc90332d74
William Ross <will@spanner.org>
2014-01-13 06:23:04 +0000

    small droom fixes

--- app/controllers/droom/organisations_controller.rb
+++ app/controllers/droom/organisations_controller.rb
@@ -3,16 +3,15 @@ module Droom
     respond_to :html, :js
     layout :no_layout_if_pjax
     helper Droom::DroomHelper
-  
     load_and_authorize_resource
 
     def create
-      @organisation.update_attributes(params[:organisation])
+      @organisation.update_attributes(organisation_params)
       respond_with @organisation
     end
 
     def update
-      @organisation.update_attributes(params[:organisation])
+      @organisation.update_attributes(organisation_params)
       respond_with @organisation
     end
 
@@ -27,8 +26,8 @@ module Droom
 
   protected
   
-    def organisation_parameters
-      params.require(:organisation).permit(:name, :description, :created_by, :owner, :url)
+    def organisation_params
+      params.require(:organisation).permit(:name, :description, :owner, :owner_id, :url)
     end
 
   end
