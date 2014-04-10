a72fdbfb78e782017f65fc66ef2c2ff86e348d17
William Ross <will@spanner.org>
2014-04-10 14:05:23 +0100

    no csrf for json signin, temporarily

--- app/controllers/droom/sessions_controller.rb
+++ app/controllers/droom/sessions_controller.rb
@@ -2,5 +2,6 @@ module Droom
   class SessionsController < Devise::SessionsController
     respond_to :html, :json
     before_filter :set_access_control_headers
+    skip_before_action :verify_authenticity_token
   end
 end
