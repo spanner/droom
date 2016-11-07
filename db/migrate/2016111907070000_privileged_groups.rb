fbe68111cc8d566c13f2d49b6931b50ce96260a7
will-r <will@spanner.org>
2016-11-07 07:11:10 +0000

    index group privilege

--- db/migrate/2016111907070000_privileged_groups.rb
+++ db/migrate/2016111907070000_privileged_groups.rb
@@ -1,5 +1,6 @@
 class PrivilegedGroups < ActiveRecord::Migration
   def change
     add_column :droom_groups, :privileged, :boolean, default: false
+    add_index :droom_groups, :privileged
   end
 end
