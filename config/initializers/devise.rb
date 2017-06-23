4b613233586312f761027c811ea9e622611c68f3
will-r <will@spanner.org>
2017-06-23 18:51:22 +0100

    set very low zxcvbn threshold by default

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,6 +17,8 @@ end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.min_password_score = 1
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
