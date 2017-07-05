1b1a74c044db0c3fed41c25402304f7b69c82460
will-r <will@spanner.org>
2017-07-05 09:33:25 +0100

    more invitation controls

--- app/controllers/droom/dashboard_controller.rb
+++ app/controllers/droom/dashboard_controller.rb
@@ -3,11 +3,11 @@ module Droom
     respond_to :html, :js
     layout :no_layout_if_pjax
     skip_authorization_check
-    
+
     def index
       authorize! :read, :dashboard
       render layout: Droom.dashboard_layout.to_s
     end
-    
+
   end
 end
