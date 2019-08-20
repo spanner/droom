22454af458e1e763362dc87c4026de8c6bf25e0a
will-r <will@spanner.org>
2019-08-20 12:05:50 +0100

    still trying to pass session_limitable checks

--- app/controllers/droom/concerns/controller_helpers.rb
+++ app/controllers/droom/concerns/controller_helpers.rb
@@ -71,6 +71,8 @@ module Droom::Concerns::ControllerHelpers
           Rails.logger.warn "✅ resource valid"
           warden.session_serializer.store(resource, :user)
           warden.session(:user)['unique_session_id'] = cookie.token               # for session_limitable
+          Rails.logger.warn "✅ stored unique_session_id: #{warden.session(:user)['unique_session_id']}"
+          Rails.logger.warn "✅ the whole session user: #{warden.session(:user).inspect}"
         end
       end
     end
