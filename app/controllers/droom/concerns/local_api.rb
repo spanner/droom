fc3afd03a7b80afba67aafa62d12eda778125d00
will-r <will@spanner.org>
2019-08-30 13:11:39 +0100

    local_api fixes

--- app/controllers/droom/concerns/local_api.rb
+++ app/controllers/droom/concerns/local_api.rb
@@ -11,14 +11,16 @@ module Droom::Concerns::LocalApi
   end
 
   def assert_local_request!
-    if (Rails.env.production? || Rails.env.staging?) && !local_request?
-      raise CanCan::AccessDenied 
-    end
+    raise CanCan::AccessDenied unless local_request?
+  end
+
+  def assert_local_request_or_signed_in!
+    raise CanCan::AccessDenied unless local_request? || user_signed_in?
   end
 
   def local_request?
     if local_subnet_defined?
-      permitted_ip_range = IPAddr.new(ENV['LOCAL_SUBNET'] || "172.0.0.0/8")
+      permitted_ip_range = IPAddr.new(ENV['LOCAL_SUBNET'])
       permitted_ip_range === IPAddr.new(request.ip)
     else
       false
@@ -26,7 +28,7 @@ module Droom::Concerns::LocalApi
   end
 
   def local_subnet_defined?
-    ENV['LOCAL_SUBNET'].present
+    ENV['LOCAL_SUBNET'].present?
   end
 
 end
