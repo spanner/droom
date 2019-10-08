dd2ecce51ae5602f19bddd1d8cdaf70a9d5806b4
will-r <will@spanner.org>
2019-10-08 14:39:11 +0100

    still looking for rogue logout in fragments controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,6 +263,7 @@ Devise.setup do |config|
   #
   Warden::Manager.before_logout do |user, warden, options|
     Rails.logger.warn "⚠️ Warden logout is unsetting after #{warden.request.fullpath}"
+    byebug
     user.clear_session_ids! if user
     Droom::AuthCookie.new(warden.cookies).unset
   end
